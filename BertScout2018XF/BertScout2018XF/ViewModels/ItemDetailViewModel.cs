cfd9fefa722336546ed9e2cff2ee64c49be1c210
Scott Bakker <scott@bakkergames.com>
2018-12-28 11:01:49 -0500

    clean xamarin tabbed project

---
+++ BertScout2018XF/BertScout2018XF/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BertScout2018XF.Models;
+
+namespace BertScout2018XF.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
